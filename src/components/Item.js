bc6502fd2b2dd8639385d222982c375a6f59faa3
Jofre Tomas <jofretomas9@gmail.com>
2024-05-02 17:19:53 -0300

    Inserindo propType  e defaultProps

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,23 @@
-function Item(props) {
+import PropTypes from "prop-types";
+
+function Item({ marca, ano_lancamento }) {
   return (
     <>
-      <li>{props.marca}</li>
+      <li>
+        {marca} - {ano_lancamento}
+      </li>
     </>
   );
 }
+
+Item.propType = {
+  marca: PropTypes.string,
+  ano_lancamento: PropTypes.number,
+};
+
+Item.defaultProps = {
+  marca: "Falto cadastra nome da Marca",
+  ano_lancamento: 0,
+};
 
 export default Item;
--- src/components/List.js
+++ src/components/List.js
@@ -5,8 +5,9 @@ function List() {
     <>
       <h1>Minha Lista</h1>
       <ul>
-        <Item marca="Fiat" />
-        <Item marca="Ford" />
+        <Item marca="Fiat" ano_lancamento={1958} />
+        <Item marca="Ford" ano_lancamento={1936} />
+        <Item marca="Ferrari" ano_lancamento={1915} />
         <Item marca="Ferrari" />
       </ul>
     </>
