b553a3fc3ba7bba6d01b07ee84af1b70edcf8f06
Jofre Tomas <jofretomas9@gmail.com>
2024-05-03 16:22:15 -0300

    Add alguns eventos aos buttons

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
-      <h1>Testando Evento</h1>
       <Evento />
-      <Form />
     </div>
   );
 }
--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,12 +1,19 @@
+import Button from "./evento/Button";
+
 function Evento() {
   function meuEvento() {
-    console.log("Fui Ativado!");
+    console.log(`Ativando primeiro evento hoje`);
+  }
+
+  function segumdoEvento() {
+    console.log(`Ativando segundo evento hoje`);
   }
 
   return (
     <div>
       <p>Clique para ativa o evento</p>
-      <button onClick={meuEvento}>Ativar</button>
+      <Button event={meuEvento} text="Primeiro Evento" />
+      <Button event={segumdoEvento} text="Segundo Evento" />
     </div>
   );
 }
