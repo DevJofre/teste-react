4751077cdf92a83b4273799dc5cbcc1d2f7c4687
Jofre Tomas <jofretomas9@gmail.com>
2024-05-03 15:20:04 -0300

    Implementação do hooks e do useState no formulario

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
+
 function Form() {
   function cadastraUsuario(event) {
     event.preventDefault();
-    console.log("Cadastrou o Usuarios!");
+
+    console.log(`Usuário ${name} foi cadastrado com a senha ${password}.`);
   }
+
+  const [name, setName] = useState();
+
+  const [password, setPassword] = useState();
 
   return (
     <div>
       <form onSubmit={cadastraUsuario}>
         <div>
-          <input type="text" placeholder="Digiti seu nome" />
+          <label htmlFor="name">Login:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digiti seu nome"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digiti sua Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
           <input type="submit" value="Cadastra" />
