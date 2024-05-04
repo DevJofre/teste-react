f06d681a7b9bb7993aaf510008242ac7fd1f63c6
Jofre Tomas <jofretomas9@gmail.com>
2024-05-04 18:22:00 -0300

    Listando itens e retornando mensagem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
-import Condicional from "./components/Condicional";
+import OutraLista from "./components/OutraLista";
 
 function App() {
+  const meusItens = ["React", "Vue", "Angular"];
+
   return (
     <div className="App">
-      <h1>Renderização</h1>
-      <Condicional />
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens} />
+      <OutraLista itens={[]} />
     </div>
   );
 }
---
+++ src/components/OutraLista.js
@@ -0,0 +1,14 @@
+function OutraLista({ itens }) {
+  return (
+    <>
+      <h3>Lista de coisas boas:</h3>
+      {itens.length > 0 ? (
+        itens.map((item, index) => <p key={index}>{item}</p>)
+      ) : (
+        <p>Não há itens na lista.</p>
+      )}
+    </>
+  );
+}
+
+export default OutraLista;
