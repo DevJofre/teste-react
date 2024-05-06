cdba824aeb9180e9087c5f54f06bf811258f061c
Jofre Tomas <jofretomas9@gmail.com>
2024-05-06 16:21:16 -0300

    Criação do icones do footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,23 @@
+import { DiAndroid } from "react-icons/di";
+import { DiGithub } from "react-icons/di";
+import { DiReact } from "react-icons/di";
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
     <div>
       <h1>Rodape</h1>
+      <ul className={styles.social_list}>
+        <li>
+          <DiAndroid />
+        </li>
+        <li>
+          <DiGithub />
+        </li>
+        <li>
+          <DiReact />
+        </li>
+      </ul>
     </div>
   );
 }
